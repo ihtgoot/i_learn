7079f0e1ed22096cc79811374bd681af8b276274
ihtgoot <ihtgoot@gmail.com>
2025-10-10 07:20:17 +0530

    day_1

---
+++ Section_3/notes.md
@@ -0,0 +1,13 @@
+# <ins>**BASIC WEB APPLICATION :**</ins>
+## **The HTTP request/response cycle :**
+    * 
+        ```
+            DNS
+            /\
+            | |
+       NAME | | IP of required web server
+            | |
+              \/     request
+            USER <================> WEB SERVER
+                    response            (monolithic)
+        ``` 
--- section_2/learn.go
+++ section_2/learn.go
@@ -8,5 +8,18 @@ func main() {
 	x = 23
 	y := 24
 	n := "fu"
-	fmt.Printf("hi there ,%d, %T, i am %s", x+y, y, n)
+	fmt.Printf("hi there ,%d, %T, i am %s\n", x+y, y, n)
+	var z int
+	a, b := outputThat(n, 797.584, x+y, &z)
+	fmt.Printf("%s, %t, %d , %p\n", a, b, z, &z)
 }
+
+func outputThat(a string, b float64, c int, d *int) (string, bool) {
+	fmt.Printf("%i %p\n", *d, d)
+	*d = 78
+	var z bool = (b == float64(c))
+	fmt.Printf("%i %p\n", *d, d)
+	var name string = a + ":fuck you"
+	return name, z
+}
+ 
