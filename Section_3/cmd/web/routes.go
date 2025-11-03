9e34a4de562864ee43c8b3edd3162dac174a71b2
ihtgoot <ihtgoot@gmail.com>
2025-11-03 17:06:31 +0530

    static files

--- Section_3/cmd/web/routes.go
+++ Section_3/cmd/web/routes.go
@@ -31,5 +31,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", labadaba)
 	mux.Get("/home", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
